be6ef9c22266e68bd2e3da436658981efd9fb738
konradbachusz <konradbachusz@gmail.com>
2021-03-01 16:51:37 +0000

    CORRECTIONS

---
+++ Lesson 5 LEFT & RIGHT Q4.sql
@@ -0,0 +1,6 @@
+SELECT SUM(vowels) vowels, SUM(other) other
+FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') 
+                        THEN 1 ELSE 0 END AS vowels, 
+          CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') 
+                       THEN 0 ELSE 1 END AS other
+         FROM accounts) t1;
