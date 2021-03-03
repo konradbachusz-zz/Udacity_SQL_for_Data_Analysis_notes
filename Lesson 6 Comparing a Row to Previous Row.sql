3584ce9e927ebb7b8c038562d3f3c2a9a1356831
konradbachusz <konradbachusz@gmail.com>
2021-03-03 11:46:28 +0000

    Comparing a Row to Previous Row

---
+++ Lesson 6 Comparing a Row to Previous Row.sql
@@ -0,0 +1,10 @@
+SELECT occurred_at,
+       total_amt_usd,
+       LEAD(total_amt_usd) OVER (ORDER BY occurred_at) AS lead,
+       LEAD(total_amt_usd) OVER (ORDER BY occurred_at) - total_amt_usd AS lead_difference
+FROM (
+SELECT occurred_at,
+       SUM(total_amt_usd) AS total_amt_usd
+  FROM orders 
+ GROUP BY 1
+) sub
