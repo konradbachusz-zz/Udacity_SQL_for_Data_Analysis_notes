49b36d8650c875bcf36b400f7ba091a0e6d10089
konradbachusz <konradbachusz@gmail.com>
2021-03-03 10:02:34 +0000

    WINDOW

---
+++ Lesson 6 Aggregates in Window Functions.sql
@@ -0,0 +1,25 @@
+SELECT id,
+       account_id,
+       standard_qty,
+       DATE_TRUNC('month', occurred_at) AS month,
+       DENSE_RANK() OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS dense_rank,
+       SUM(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS sum_std_qty,
+       COUNT(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS count_std_qty,
+       AVG(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS avg_std_qty,
+       MIN(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS min_std_qty,
+       MAX(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS max_std_qty
+FROM orders
+
+
+--Removed ORDER BY DATE_TRUNC('month',occurred_at)
+SELECT id,
+       account_id,
+       standard_qty,
+       DATE_TRUNC('month', occurred_at) AS month,
+       DENSE_RANK() OVER (PARTITION BY account_id ) AS dense_rank,
+       SUM(standard_qty) OVER (PARTITION BY account_id ) AS sum_std_qty,
+       COUNT(standard_qty) OVER (PARTITION BY account_id ) AS count_std_qty,
+       AVG(standard_qty) OVER (PARTITION BY account_id ) AS avg_std_qty,
+       MIN(standard_qty) OVER (PARTITION BY account_id ) AS min_std_qty,
+       MAX(standard_qty) OVER (PARTITION BY account_id ) AS max_std_qty
+FROM orders
---
+++ Lesson 6 Aliases for Multiple Window Functions.sql
@@ -0,0 +1,12 @@
+SELECT id,
+       account_id,
+       DATE_TRUNC('year',occurred_at) AS year,
+       DENSE_RANK() OVER account_year_window AS dense_rank,
+       total_amt_usd,
+       SUM(total_amt_usd) OVER account_year_window AS sum_total_amt_usd,
+       COUNT(total_amt_usd) OVER account_year_window AS count_total_amt_usd,
+       AVG(total_amt_usd) OVER account_year_window AS avg_total_amt_usd,
+       MIN(total_amt_usd) OVER account_year_window AS min_total_amt_usd,
+       MAX(total_amt_usd) OVER account_year_window AS max_total_amt_usd
+FROM orders
+WINDOW account_year_window AS (PARTITION BY account_id ORDER BY DATE_TRUNC('year',occurred_at))
